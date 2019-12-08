32fb7d71da9cfb2a34687ff77128cb95e9508e91
José Luis Martín Muñoz <titojlmm@gmail.com>
2019-12-08 01:10:42 +0100

    Ejercicios del capítulo 1: Computers and Programming

---
+++ Chapter1_ComputersAndProgramming/1_02_ComputeCircumference.py
@@ -0,0 +1,6 @@
+
+print ("This program finds the circumference of a circle.")
+radius = input("Enter the radius of the circle: ")
+radius = float(radius)
+circ = radius * 2.0 * 3.1415926
+print ("The circumference is ", circ)
